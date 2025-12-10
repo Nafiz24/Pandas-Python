6c61a111c074cd746163f221b351db31d5213f04
Nafuu <nafzplayz@gmail.com>
2025-12-10 19:07:16 +0600

    Added Some DataFrame to it

--- 8_CategoricalData/demo1.py
+++ 8_CategoricalData/demo1.py
@@ -1,1 +1,6 @@
+#Create Categorial Series By In Pandas
 import pandas as pd 
+
+s = pd.Series(data=["p" , "q" , "r" , "s" , "t"] , dtype='category')
+
+print("Series: \n" , s)
---
+++ 8_CategoricalData/demo2.py
@@ -0,0 +1,8 @@
+# Create a acategorial Data Frame in Pandas...
+
+import pandas as pd  
+df = pd.DataFrame({"Cat1" : list("pop") , "Cat2" : list("ppp"),"Cat3" : list("pap")} ,dtype = 'category')
+
+#Display the DataFrame 
+
+print("FataFrame: \n" , df)
